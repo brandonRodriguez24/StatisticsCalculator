bd47aeed5d49c5e497cae7da574f82a45c73bbe6
brandonRodriguez24 <55503010+brandonRodriguez24@users.noreply.github.com>
2020-03-07 17:19:41 -0500

    Add files via upload

---
+++ RandomGenerator/numbersFromSeedList.py
@@ -0,0 +1,11 @@
+import random
+from RandomGenerator.numbersFromList import PickNoSeed
+
+class PickNumbersSeed():
+    @staticmethod
+    def pickNumbers(theSeed, aList, rangeNum):
+        random.seed(theSeed)
+
+        newList = PickNoSeed.pickNumbers(aList, rangeNum)
+        return newList
+
